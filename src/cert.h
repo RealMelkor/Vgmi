139a6b6d6eb9b5597911cbef942721c09a429d70
Vaati <vaati@openbsd.my.domain>
2022-04-09 23:48:17 -0400

    add unveil and pledge on OpenBSD

--- src/cert.h
+++ src/cert.h
@@ -6,6 +6,7 @@
 
 int cert_create(char* url);
 int cert_getpath(char* host, char* crt, int crt_len, char* key, int key_len);
+int gethomefolder(char* path, size_t len);
 int getcachefolder(char* path, size_t len);
 
 #endif
