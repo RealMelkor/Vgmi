aac742b8d1b4721191ae9fb39cbdec165cfe09a4
Lathas <lathas@Kandarin.gielinor.lan>
2022-05-24 18:21:45 -0400

    fix xdg disabled preprocessor

--- src/cert.h
+++ src/cert.h
@@ -7,7 +7,7 @@
 
 int cert_create(char* url);
 int cert_getpath(char* host, char* crt, int crt_len, char* key, int key_len);
-int cert_verify(char* host, const char* hash, time_t start, time_t end);
+int cert_verify(char* host, const char* hash, unsigned long long start, unsigned long long end);
 int gethomefolder(char* path, size_t len);
 int getcachefolder(char* path, size_t len);
 int getdownloadfolder(char* path, size_t len);
