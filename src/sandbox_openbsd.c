77b6b06774b2f0a4fdc601c898331028f510455b
Raw Monk <realmelkor@proton.me>
2023-10-12 21:32:56 -0400

    fix openbsd sandbox

--- src/sandbox_openbsd.c
+++ src/sandbox_openbsd.c
@@ -1,4 +1,6 @@
 #ifdef __OpenBSD__
+#include <stdio.h>
+#include <unistd.h>
 #include "macro.h"
 #include "sandbox.h"
 #include "storage.h"
@@ -10,8 +12,13 @@ int sandbox_init() {
 	int ret;
 
 	if ((ret = storage_path(V(path)))) return ret;
+	if (unveil("/etc/resolv.conf", "r")) return ERROR_SANDBOX_FAILURE;
+	if (unveil("/etc/hosts", "r")) return ERROR_SANDBOX_FAILURE;
 	if (unveil(path, "rwc")) return ERROR_SANDBOX_FAILURE;
 	if (unveil(NULL, NULL)) return ERROR_SANDBOX_FAILURE;
+
+	if (pledge("tty stdio inet dns rpath wpath cpath", ""))
+		return ERROR_SANDBOX_FAILURE;
 
 	return 0;
 }
